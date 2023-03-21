45847a145f645431e9a7acf5530ef267a2c956e8
AtleySt <atleystvrain@gmail.com>
2023-03-21 12:35:12 -0500

    getGameStatus in progress

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -25,7 +25,26 @@ public class TicTacToeBoard {
     }
 
     public int getGameStatus() {
-        if ((board()[0][0] == 1 && board()[0][1] == 1))
+        if ((board()[0][0] == 1 && board()[0][1] == 1 && board()[0][2] == 1)
+         || (board()[1][0] == 1 && board()[1][1] == 1 && board()[1][2] == 1)
+          || (board()[2][0] == 1 && board()[2][1] == 1 && board()[2][2] == 1)
+          || (board()[0][0] == 1 && board()[1][0] == 1 && board()[2][0] == 1)
+          || (board()[0][1] == 1 && board()[1][1] == 1 && board()[2][1] == 1)
+          || (board()[0][2] == 1 && board()[1][2] == 1 && board()[2][2] == 1)
+          || (board()[0][0] == 1 && board()[1][1] == 1 && board()[2][2] == 1) ||
+          (board()[0][2] == 1 && board()[1][1] == 1 && board()[2][0] == 1)) {
+            return 1;
+          } else if ((board()[0][0] == 2 && board()[0][1] == 2 && board()[0][2] == 2)
+          || (board()[1][0] == 2 && board()[1][1] == 2 && board()[1][2] == 2)
+           || (board()[2][0] == 2 && board()[2][1] == 2 && board()[2][2] == 2)
+           || (board()[0][0] == 2 && board()[1][0] == 2 && board()[2][0] == 2)
+           || (board()[0][1] == 2 && board()[1][1] == 2 && board()[2][1] == 2)
+           || (board()[0][2] == 2 && board()[1][2] == 2 && board()[2][2] == 2)
+           || (board()[0][0] == 2 && board()[1][1] == 2 && board()[2][2] == 2) ||
+           (board()[0][2] == 2 && board()[1][1] == 2 && board()[2][0] == 2)) {
+             return 2;
+           } else if ()
+
     }
 
     public int getCellContents(int r, int c) {
