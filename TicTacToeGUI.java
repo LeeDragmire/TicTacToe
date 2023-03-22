30474b7f6197c73a72236653db66248d0cc9bab3
colton <adamscolton64@gmail.com>
2023-03-22 12:04:21 -0500

    functional no restart

--- TicTacToeGUI.java
+++ TicTacToeGUI.java
@@ -4,16 +4,50 @@ import java.awt.*;
 import javax.swing.*;
 
 public class TicTacToeGUI extends JFrame{
+    TicTacToeBoard theBoard;
+    private static int player = 1;
+
+
     public TicTacToeGUI() {
+        theBoard = new TicTacToeBoard();
+        
+
         this.setSize(500, 600);
         this.setPreferredSize(new Dimension(500, 600));
         this.setTitle("Tic Tac Toe");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.add(new TicTacToePanel());
         this.setVisible(true);
+        this.setResizable(false);
     }
 
     public class TicTacToePanel extends JPanel {
+        private static void buttonPress(JButton button, JLabel text, TicTacToeBoard theBoard, int r, int c) {
+             if (theBoard.isValidMove(r, c)) {
+                    theBoard.makeMove(r, c, player);
+                    if (player == 1) {
+                        button.setText("X");
+                        text.setText("Player 2 place an O");
+                        player++;
+                    } else {
+                        button.setText("O");
+                        text.setText("Player 1 place an X");
+                        player--;
+                    }
+                } else {
+                    text.setFont(new Font("Serif", Font.PLAIN, 40));
+                    text.setText("Please Make Valid Move");
+                }
+            
+            if (theBoard.getGameStatus() == 1) {
+                text.setText("Player 1 Wins");
+            } else if (theBoard.getGameStatus() == 2) {
+                text.setText("Player 2 Wins");
+            } else if (theBoard.getGameStatus() == -1) {
+                text.setText("It's a Tie");
+            }
+        }
+
         public TicTacToePanel() {
             this.setSize(500, 600);
             this.setPreferredSize(new Dimension(500, 600));
@@ -29,31 +63,31 @@ public class TicTacToeGUI extends JFrame{
             JButton bottomRight = new JButton("");
             JLabel text = new JLabel("Player 1 place an X");
             topLeft.addActionListener(e -> {
-                
+                buttonPress(topLeft, text, theBoard, 0, 0);
             });
             topMiddle.addActionListener(e -> {
-                
+                buttonPress(topMiddle, text, theBoard, 0, 1);
             });
             topRight.addActionListener(e -> {
-                
+                buttonPress(topRight, text, theBoard, 0, 2);
             });
             middleLeft.addActionListener(e -> {
-                
+                buttonPress(middleLeft, text, theBoard, 1, 0);
             });
             middle.addActionListener(e -> {
-                
+                buttonPress(middle, text, theBoard, 1, 1);
             });
             middleRight.addActionListener(e -> {
-                
+                buttonPress(middleRight, text, theBoard, 1, 2);
             });
             bottomLeft.addActionListener(e -> {
-                
+                buttonPress(bottomLeft, text, theBoard, 2, 0);
             });
             bottomMiddle.addActionListener(e -> {
-                
+                buttonPress(bottomMiddle, text, theBoard, 2, 1);
             });
             bottomRight.addActionListener(e -> {
-                
+                buttonPress(bottomRight, text, theBoard, 2, 2);
             });
             
             this.add(topLeft);
